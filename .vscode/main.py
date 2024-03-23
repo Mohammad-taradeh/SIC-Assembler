91a2640f97e0b635e2127dad17f7b1e482f3af3b
Mohammad-taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-03-23 23:32:54 +0200

    SCI Assymbler Pass One Final version

---
+++ .vscode/main.py
@@ -0,0 +1,13 @@
+# Welcome to the SIC project for the Systems Programming course
+# Supervised by: Dr. Yousef Salah
+# Group canditates:
+# Mohammad Taradeh - 201160
+
+import re
+import ToolsFile
+import pass_one
+
+mainArray = ToolsFile.appendall()
+pass_one.locationCounter()
+pass_one.SymbolTable()
+
